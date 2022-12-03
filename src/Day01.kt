5e5dffb2dc5212ffc1955c3d75bc29b90ca9ed13
kuolemax <kuolemax@qq.com>
2022-12-03 18:07:34 +0800

    feat: day1 part2 pass.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun countPerCalories(input: List<String>): MutableList<Int> {
         val arr = mutableListOf<Int>()
         var index = 0
         var newStart = 0
@@ -20,18 +20,26 @@ fun main() {
                 newStart = index
             }
         }
+        return arr
+    }
+
+    fun part1(input: List<String>): Int {
+        val arr = countPerCalories(input)
         return arr.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = countPerCalories(input)
+        calories.sortDescending()
+        return calories.subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
